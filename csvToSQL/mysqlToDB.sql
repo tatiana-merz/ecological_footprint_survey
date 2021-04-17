369f65d42330f018e324353ef820dc2b6c018c6c
tatiana-merz <73852793+tatiana-merz@users.noreply.github.com>
2021-04-17 19:53:23 +0200

    db upload

---
+++ csvToSQL/mysqlToDB.sql
@@ -0,0 +1,8 @@
+USE EcoDB;
+INSERT INTO Eco (Name,Age,Email,Role,Comments,Shower,Flush,Brush,Wash,Savingtoilets,Showerheads,Dishwasher) VALUES
+("Tania","25","iscream1123@gmail.com","student","no","3-6minuteslong","everytime","don't let water run","No","No","No","No")
+,("Tatiana Merzhevich","34","fmfgerard@gmail.com","full time job","34","Noshower","everytime","let water run","Yes","Yes","Yes","Yes")
+,("Tatiana Merzhevich","44","tanuk195@mail.ru","other","nooooo","10ormoreminuteslong","Sometimes","don't let water run","No","No","Yes","No")
+,("test5","55","iscream1123@gmail.com","part time job","test5","Noshower","everytime","let water run","Yes","Yes","Yes","Yes")
+,("test6","55","79111165395@mail.ru","full time job","test6","Noshower","everytime","let water run","Yes","Yes","Yes","Yes")
+;
