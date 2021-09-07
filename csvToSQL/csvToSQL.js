f881ba442f79821d660d1512debe5268f7b8c6a3
tatiana-merz <tatiana.merzhevich@gmal.com>
2021-09-07 19:16:12 +0200

    finishing sqlite

--- csvToSQL/csvToSQL.js
+++ csvToSQL/csvToSQL.js
@@ -1,13 +1,13 @@
 //csv_stream.js
 var fs = require('fs');
 var rstream = fs.createReadStream('../Data/responses.csv');
-var wstream = fs.createWriteStream('mysqlToDB.sql');
+var wstream = fs.createWriteStream('Responses.sql');
 
 
 var CSV2SQL = require('csv2sql-lite');
 var csv2sql = CSV2SQL({
-    tableName: 'Eco_dg_tmp',
-    dbName: 'EcoDB',
+    tableName: 'data',
+    dbName: 'Responses',
     separator: ',',
     lineSeparator: ','
 });
